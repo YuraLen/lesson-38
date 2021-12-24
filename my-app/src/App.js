1b92d7fbae08af924d36578abcd09e27e936bcb3
YuraLen <93992829+Roman777R@users.noreply.github.com>
2021-12-24 20:39:51 +0200

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import './App.css';
-import {SelectCom} from "./components/select"
-import {SliderCom} from "./components/slider"
-import {RadioGroupCom} from "./components/radioGrup"
-import {DemoCarousel} from "./components/carousel"
-import {CalendarCom} from "./components/calendar"
+import { SelectCom } from "./components/select"
+import { SliderCom } from "./components/slider"
+import { RadioGroupCom } from "./components/radioGrup"
+import { DemoCarousel } from "./components/carousel"
+import { CalendarCom } from "./components/calendar"
+// import { ClockComp } from "./components/clock"
 
 export const App = () => {
-  return(
-    <div style={{margin: "20px"}}>
-      <SelectCom/>
-      <SliderCom/>
-      <RadioGroupCom/>
-      <DemoCarousel/>
-      <CalendarCom/>
+  return (
+    <div style={{ margin: "20px" }}>
+      <SelectCom />
+      <SliderCom />
+      <RadioGroupCom />
+      <DemoCarousel />
+      <CalendarCom />
+      {/* <ClockComp/> */}
     </div>
   )
 }
---
+++ my-app/src/components/clock.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import Clock from 'react-live-clock';
+
+// export const ClockComp = () => {
+//   return (
+//     <div>
+//       <h1>
+//         <Clock format="HH:mm:ss" interval={1000} ticking={true} />
+//       </h1>
+//     </div>
+//   )
+// }
